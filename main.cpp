b1e7570dbc3eb82b8979a00c2a5313ba6cff7663
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-26 00:25:24 +0200

    Update main

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    sf::RectangleShape rect(sf::Vector2f(400, 300));
-    rect.setFillColor(sf::Color::Red);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -15,8 +11,7 @@ int main() {
         }
 
         window.clear();
-        window.draw(shape);
-        window.draw(rect);
+
         window.display();
     }
 
